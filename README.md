c7a419cbc6514e294d355ba81ae56092b4a9c6de
paul <116600015+paulstllb@users.noreply.github.com>
2023-10-06 23:59:11 +0200

    Raspberry Bibliothek/I2C

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Rüber ziehen mit: scp -r /Users/paulstallbr/Documents/GitHub/VVS paulstallbr@pi.local:/home/paulstallbr/
+2. rm -r node-modules
+3. npm i 
--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const BASE_URL = "https://www3.vvs.de/mngvvs/XML_DM_REQUEST?";
 const fs  = require('fs');
+const LCD = require('raspberrypi-liquid-crystal');
+const lcd = new LCD( 1, 0x27, 16, 2 );
+lcd.beginSync();
+setInterval(()=>update(),1000*20);
+
+function update(){
 const apiUrl = retrieveStationData(6503,0)
 
 fetch(apiUrl)
@@ -10,15 +16,20 @@ fetch(apiUrl)
     fs.writeFile('./test.json',JSON.stringify(data),()=>{})
     let daata = new UBahnInfo(data);
     let newArray = daata.getDepartureTimes('U2').concat(daata.getDepartureTimes('U29'),daata.getDepartureTimes('91'))
-    console.log(newArray.sort((a,b)=>a[1]-b[1]))
+    let DatenArray = newArray.sort((a,b)=>a[1]-b[1]);
+    lcd.clearSync();
+    lcd.printSync(DatenArray[0][0].toString()+"  "+DatenArray[0][1].getHours().toString()+":"+DatenArray[0][1].getMinutes().toString()+"  "+DatenArray[0][2].toString());
+    lcd.setCursorSync(0, 1);
+    lcd.printSync(DatenArray[1][0].toString()+ "  "+DatenArray[1][1].getHours().toString()+":"+DatenArray[1][1].getMinutes().toString()+ "  "+ DatenArray[1][2].toString());
     return newArray.sort((a,b)=>a[1]-b[1])
   })
   .catch(error => {
     console.error('Fehler bei der Anfrage:', error);
   });
+    
+  
 
-
-
+}
 class UBahnInfo {
     constructor(data) {
         this.data = data;

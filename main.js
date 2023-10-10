5e51067509ceff6a10d37bfc1274ba9c64b58d56
paul <116600015+paulstllb@users.noreply.github.com>
2023-10-11 00:03:18 +0200

    Debug versuch der API

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const BASE_URL = "https://www3.vvs.de/mngvvs/XML_DM_REQUEST?";
 const fs  = require('fs');
+
+//LCD Bildschirm 
+/*
 const LCD = require('raspberrypi-liquid-crystal');
 const lcd = new LCD( 1, 0x27, 16, 2 );
 lcd.beginSync();
+*/
 setInterval(()=>update(),1000*10);
+
 
 function update(){
 const apiUrl = retrieveStationData(6503,0)
-
+console.log(apiUrl);
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
@@ -17,11 +22,15 @@ fetch(apiUrl)
     let daata = new UBahnInfo(data);
     let newArray = daata.getDepartureTimes('U2').concat(daata.getDepartureTimes('U29'),daata.getDepartureTimes('91'))
     let DatennArray = newArray.sort((a,b)=>a[1]-b[1]);
-    let DatenArray = DatennArray.slice(0,2);
+    //let DatenArray = DatennArray.slice(0,2);
+    console.log(DatennArray);
+    //LCD Hardware 
+    /*
     lcd.clearSync();
     lcd.printSync(DatenArray[0][0].toString()+"  "+DatenArray[0][1].getHours().toString()+":"+DatenArray[0][1].getMinutes().toString()+"  "+DatenArray[0][2].toString());
     lcd.setCursorSync(0, 1);
     lcd.printSync(DatenArray[1][0].toString()+ "  "+DatenArray[1][1].getHours().toString()+":"+DatenArray[1][1].getMinutes().toString()+ "  "+ DatenArray[1][2].toString());
+    */
     return newArray.sort((a,b)=>a[1]-b[1])
   })
   .catch(error => {
@@ -75,7 +84,7 @@ function retrieveStationData(stationId, offset) {
         url += `&itdDateDay=` + d.getDate().toString();
         url += `&itdTimeHour=` + d.getHours().toString();
         url += `&itdTimeMinute=` + d.getMinutes().toString();
-         
+        
     }
     return url;
 }
